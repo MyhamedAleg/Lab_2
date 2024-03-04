040b16a96b63b00408597c9907effb399051132c
Myhamed Aleg <oleh.lata.tr.2022@lpnu.ua>
2024-03-04 11:59:52 +0200

    4.2(1)

---
+++ src/Operator.java
@@ -0,0 +1,33 @@
+public class Operator {
+    public static void main(String[] args) {
+       int day = 7 ;
+       switch (day){
+           case 1:
+               System.out.println("Monday workday");
+               break;
+           case 2:
+               System.out.println("Tuesday workday");
+               break;
+               case 3:
+               System.out.println("Wednesday workday");
+               break;
+           case 4:
+               System.out.println("Thursday  workday");
+               break;
+           case 5:
+               System.out.println("Friday workday");
+               break;
+           case 6:
+               System.out.println("Saturday day off");
+               break;
+           case 7:
+               System.out.println("Sunday day off");
+               break;
+
+       }
+
+
+
+}
+    }
+
