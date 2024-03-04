79b034230e508c69a1fcecd703e2ca6baa4d37c8
Myhamed Aleg <oleh.lata.tr.2022@lpnu.ua>
2024-03-04 12:14:38 +0200

    4.2(2)

---
+++ src/Operator2.java
@@ -0,0 +1,28 @@
+public class Operator2 {
+    public static void main(String[] args) {
+        int mounth = ;
+        switch (mounth) {
+            case 1:
+        case 2:
+            case 3 :
+                System.out.println("I`ts winter");
+                break;
+            case 4:
+        case 5 :
+        case 6:
+                System.out.println("I`ts Spring");
+                break;
+            case 7:
+        case 8 :
+        case 9:
+                System.out.println("I`ts Summer");
+                break;
+            case 10:
+        case 11:
+        case 12:
+                System.out.println("I`ts  Autumn ");
+                break;
+
+        }
+    }
+}
